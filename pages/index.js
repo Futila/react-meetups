0ea55cc7aadeb864a76cf4118e27a5d6accb28ca
Fernando Futila <fernando.futila@gmail.com>
2022-09-06 07:42:54 -0300

    Outputting a List of Meetups

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First meetup",
+    image: "https://avatars.githubusercontent.com/u/86152008?v=4",
+    address: "Some address 220, name city",
+    description: "This a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second meetup",
+    image: "https://avatars.githubusercontent.com/u/86152008?v=4",
+    address: "Some address 220, name city",
+    description: "This a second meetup!",
+  },
+];
+
 export default function HomePage() {
-  return <h1>Homepage</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
