b8320ec08c010afa7ff3d73a775163e4737bc82c
Fernando Futila <fernando.futila@gmail.com>
2022-09-07 11:13:32 -0300

    Data Fecthing for static pages

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function DetailsPage() {
+  return (
+    <MeetupDetail
+      id="m1"
+      image="https://avatars.githubusercontent.com/u/86152008?v=4"
+      title="A First meetup"
+      address="Some address 220, name city"
+      description="This a first meetup!"
+    />
+  );
+}
