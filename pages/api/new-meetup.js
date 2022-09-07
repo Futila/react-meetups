022788fe054a9dc3de08cb22ae552cf093842bf7
Fernando Futila <fernando.futila@gmail.com>
2022-09-07 17:36:38 -0300

    Working with mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,26 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    //const { title, image, address, description } = data;
+
+    client = await MongoClient.connect(
+      "mongodb+srv://fernando_futila:fernando123@cluster0.jnthl8v.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
+
+export default handler;
